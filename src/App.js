ef71b876566a6a156f364b242c28edc295b0f3f3
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-09-24 20:10:32 -0600

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import StoriesContainer from './Components/StoriesContainer/StoriesContainer';
+import { Link, Route } from 'react-router-dom';
 
 function App() {
   return (
     <nav>
-      <h1>NY Times News Reader</h1>
-      <StoriesContainer />
+      <Link to={'/'} style={{textDecoration: 'none'}}>
+        <h1>NY Times News Reader</h1>
+      </Link>
+      <Route 
+        exact path='/' render={() => {
+          return (
+            <StoriesContainer />
+          )
+        }}
+      />
     </nav>
   );
 }
