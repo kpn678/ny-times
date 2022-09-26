261b5e537991bc0ec97cb0279adb978ca8662397
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-09-25 23:12:32 -0600

    Make GET call dynamic

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
-const getStories = async () => {
-  const response = await fetch ('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=Sip1GXvI9tspXaACu4xTIT2D6UgAAWTI');
+const getStories = async (topic) => {
+  const response = await fetch (`https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=Sip1GXvI9tspXaACu4xTIT2D6UgAAWTI`);
   const listOfStories = await response.json();
   return listOfStories;
 }
