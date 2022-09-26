e04e4845b7fd59b1ba6751918b82cd492452eae7
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-09-25 23:53:30 -0600

    Create topics

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,13 +2,17 @@ import React from "react";
 import './NavBar.css';
 import { Link } from 'react-router-dom';
 
-
-const NavBar = () => {
+const NavBar = ({ updateStories }) => {
   return (
     <nav>
       <Link to={'/'} style={{textDecoration: 'none'}}>
         <h1>NY Times News Reader</h1>
       </Link>
+      <section className='topics-container'>
+        <p className='home' onClick={() => {updateStories('home')}}>Home</p>
+        <p className='topic' onClick={() => {updateStories('business')}}>Business</p>
+        <p className='topic' onClick={() => {updateStories('health')}}>Health</p>
+      </section>
     </nav>
   );
 }
