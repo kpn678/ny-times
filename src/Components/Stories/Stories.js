d153064a3218a742c200d54eb0f6021d6418d298
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-09-26 17:22:25 -0600

    Add underline and spacing to each story card

--- src/Components/Stories/Stories.js
+++ src/Components/Stories/Stories.js
@@ -10,7 +10,7 @@ const Stories = ({ story, loadDetails }) => {
         <img src={story.multimedia[2].url} height={story.multimedia[2].height} width={story.multimedia[2].width}/>
       }
       <Link to={'/details'} style={{textDecoration: 'none'}}>
-        <h3 onClick={() => {loadDetails(story.title)}}>{story.title}</h3>
+        <h3 className='story-title' onClick={() => {loadDetails(story.title)}}>{story.title}</h3>
       </Link>
       <p>{story.byline}</p>
       <p>Published: {story.created_date.substr(0, 10)}</p>
